042e5eca01588cf7b579f6b5276c3c0eb303504d
wz <318458563@qq.com>
2021-03-01 19:14:47 +0800

    initCDG基本完成

--- src/initCDG.cpp
+++ src/initCDG.cpp
@@ -6,23 +6,41 @@
 using namespace SVF;
 using namespace llvm;
 using namespace std;
-void findSSet(ICFGNode* entryNode,Set<const ICFGNode*> visited);
+
+typedef DomTreeNodeBase<llvm::BasicBlock>* DTNodeTy;
+typedef struct {
+    DTNodeTy A;
+    DTNodeTy B;
+    DTNodeTy L;
+    NodeID TF;
+}DepenTupleTy;
+typedef Set<DepenTupleTy*> DepenSSetTy;
+typedef vector<DTNodeTy> DepenVecTy;
+
+void findSSet(ICFGNode* entryNode,Set<const ICFGNode*> &visited,DepenSSetTy &setS);
+void buildinitCDG(DepenSSetTy S);
 u32_t icfgOutEdgeNum(ICFGNode* iNode);
-bool hasPostDom(DomTreeNodeBase<llvm::BasicBlock>* A,DomTreeNodeBase<llvm::BasicBlock>* B);
+
+void findPathA2B(DTNodeTy A,DepenSSetTy S,vector <DTNodeTy> &P);
+void findPathL2B(DepenSSetTy S,vector <DTNodeTy> &P);
+void handleDepenVec(DepenTupleTy* LB,vector <DTNodeTy> &P);
 
 llvm::PostDominatorTree *PT= nullptr;
+
 void CDG::initCDG(SVFFunction *fun ) {
 
     ICFG* icfg;
     PT->recalculate(*fun->getLLVMFun());
-    ICFGNode* iNode= icfg->getFunEntryBlockNode(fun);
-    ICFGNode* exitNode=icfg->getFunExitBlockNode(fun);
+
+    const BasicBlock* exbb=SVFUtil::getFunExitBB(fun->getLLVMFun());
+    const BasicBlock* enbb=&(fun->getLLVMFun()->front());
 
     Set<const ICFGNode*> visited;
-    findSSet(iNode,visited);
-
+    DepenSSetTy setS;
+    findSSet(icfg->getFunEntryBlockNode(fun),visited,setS);
+    buildinitCDG(setS);
 }
-void findSSet(ICFGNode* iNode,Set<const ICFGNode*> visited){
+void findSSet(ICFGNode* iNode,Set<const ICFGNode*> &visited,DepenSSetTy &setS){
     if (visited.find(iNode) == visited.end())
         visited.insert(iNode);
     else
@@ -31,41 +49,67 @@ void findSSet(ICFGNode* iNode,Set<const ICFGNode*> visited){
         return;
     if(icfgOutEdgeNum(iNode)!=2){
         for (ICFGEdge* edge : iNode->getOutEdges())
-            findSSet(edge->getDstNode(),visited);
+            findSSet(edge->getDstNode(),visited,setS);
         return;
     }
-    //handle edges with condition
+    //handle edges with condition to get S
+    //traverse posdomtree to get P
     for (ICFGNode::const_iterator it = iNode->OutEdgeBegin(), eit =
             iNode->OutEdgeEnd(); it != eit; ++it)
     {
         IntraCFGEdge* edge= SVFUtil::dyn_cast<IntraCFGEdge>(*it);
         if (edge)
             continue;
-        findSSet(edge->getDstNode(),visited);
+        findSSet(edge->getDstNode(),visited,setS);
         NodeID TF = edge->getBranchCondtion().second;
-        pair<IntraCFGEdge *, NodeID> S(edge, TF);
         const llvm::BasicBlock* bbA= edge->getSrcNode()->getBB();
         const llvm::BasicBlock* bbB= edge->getSrcNode()->getBB();
-        DomTreeNodeBase<llvm::BasicBlock>* domNodeA=PT->getNode(bbA);
-        DomTreeNodeBase<llvm::BasicBlock>* domNodeB=PT->getNode(bbB);
-        if (!hasPostDom(domNodeA,domNodeB)){
-            //@todo: 这里需要一个低成本的遍历方法找到从B到A父节点的路径
-//            traveBackPostDom();
+        //判断改边的两个点是否存在支配关系，存在则存入S集合
+        DTNodeTy dnA=PT->getNode(bbA);
+        DTNodeTy dnB=PT->getNode(bbB);
+        if (!PT->properlyDominates(dnA,dnB)){//Return true iff B dominates A and A != B.
+            DTNodeTy dnL=PT->getNode(PT->findNearestCommonDominator(bbA,bbB));
+            DepenTupleTy ABLT={dnA,dnB,dnL,TF};
+            setS.insert(&ABLT);
         }
     }
 }
-bool traveBackPostDom(DomTreeNodeBase<llvm::BasicBlock>* root){
-    for (auto it=root->begin(),ie=root->end();it!=ie;++it);
-    return true;
+void findPathA2B(DTNodeTy A,DepenSSetTy S,vector <DTNodeTy> &P){
+    P.push_back(A);
+    for(auto it=S.begin(),ie=S.end();it!=ie;it++){
+        if(A==(*it)->B){
+            handleDepenVec(*it,P);
+            S.erase(it);
+            break;
+        }
+    }
+    for (auto it=A->begin(),ie=A->end();it!=ie;++it)
+        findPathA2B(*it,S,P);
+    P.pop_back();
+}
+void findPathL2B(const DepenSSetTy S,vector <DTNodeTy> &P){
+    findPathA2B(PT->getRootNode(),S,P);
 }
 
-
-bool hasPostDom(DomTreeNodeBase<llvm::BasicBlock>* A,DomTreeNodeBase<llvm::BasicBlock>* B){
-    for (auto it=B->begin(),ie=B->end();it!=ie;++it)
-        if(B==A)
-            return false;
-    return true;
+void buildinitCDG(DepenSSetTy S){
+    vector <DTNodeTy> vecP;
+    findPathL2B(S,vecP);
 }
+void handleDepenVec(DepenTupleTy* LB,vector <DTNodeTy> &P){
+    DTNodeTy L=LB->L,B=LB->B,A=LB->A;
+    auto pi=std::find(P.begin(),P.end(),L);//find L
+    if(PT->properlyDominates(L,B))//if A is the ancestor of B,then A==L the path contant L,otherwise dont contain
+        if (pi==P.end())
+            return;
+        else
+            ++pi;
+    for(;pi!=P.end();pi++){
+        //@todo::添加节点和边
+//        addNode((*pi)->getBlock());
+//        addEdge(A,*(pi))
+    }
+}
+
 
 u32_t icfgOutEdgeNum(ICFGNode* iNode){
     u32_t n=0;
